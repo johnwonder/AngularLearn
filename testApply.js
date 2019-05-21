ef4cb7e805755e94c707e619eb34069fdd3bc1d2
johnwonder <johnufida@163.com>
2019-05-21 15:15:54 +0800

    ngResource

--- testApply.js
+++ testApply.js
@@ -34,5 +34,31 @@ var provider = {
 
 console.log(applyFunction1.apply(provider,args));
 
+
+ url = "http://url.com/id./format?q=x".replace(/\.\/(?=\w+($|\?))/, '.');
+
+ console.log(url);
 //输出 key:11 value:22
+
+var testCase = 4;
+ switch (testCase) {
+              case 4:
+                 console.log(4);
+              //fallthrough
+              case 3:
+               console.log(4);
+              case 2:
+                if (false) {
+                 	
+                } else {
+                    console.log("break");
+                  break;
+                }
+              case 1:
+                 
+                break;
+              case 0: break;
+              default:
+                 break;
+}
 
